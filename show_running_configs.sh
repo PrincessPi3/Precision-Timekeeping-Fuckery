343c7a5c0eb049eedce1582a776c35dd507351ef
PrincessPi3 <human.bagel@gmail.com>
2025-06-13 17:50:05 -0600

    1749858606

---
+++ show_running_configs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+less /etc/default/gpsd
+less /etc/chrony/conf.d/precision_timekeeping.conf
+less /etc/grafana/grafana.ini
+less /etc/influxdb/influxdb.conf
+less /etc/telegraf/telegraf.conf
+less /etc/udev/rules.d/50-tty.rules
+less /boot/firmware/config.txt
+less /lib/udev/hwclock-set
