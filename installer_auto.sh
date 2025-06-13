e9c8ed808ade74299f71c59751059a696fd9861c
PrincessPi3 <human.bagel@gmail.com>
2025-06-13 17:18:57 -0600

    1749856737

--- installer_auto.sh
+++ installer_auto.sh
@@ -4,3 +4,7 @@
 set -e
 sudo apt update
 sudo apt install git -y
+cd $HOME
+git clone https://github.com/PrincessPi3/Precision-Timekeeping-Fuckery.git
+cd Precision-Timekeeping-Fuckery
+bash installer1.sh
