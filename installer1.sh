02daa57524d7648ad309c8d19af4bddfbc1fd8c2
PrincessPi3 <human.bagel@gmail.com>
2025-06-13 16:02:17 -0600

    1749852137

--- installer1.sh
+++ installer1.sh
@@ -15,8 +15,9 @@ sudo rpi-update
 
 # run da raspberry pi config script
 echo "Configure Raspberry Pi..."
-echo -e "Enable I2C Support in raspi-config\n\tInterface Options->I2C->\n\tenable <Yes>\n\tkernel module loaded by default <Yes>\n\nInterface Options->Serial Port\n\tWould you like a login shell to be accessible over       │
-                              │ serial? <No>\n\tWould you like the serial port hardware to be enabled? <Yes>"
+echo -e "Enable I2C Support in raspi-config\n\tInterface Options->I2C->\n\t Would you like the ARM I2C interface to be enabled? <Yes>\n\tkernel module loaded by default <Yes>\n\nInterface Options->Serial Port\n\tWould you like a login shell to be accessible over       │
+                              │ serial? <No>\n\tWould you like the serial port hardware to be enabled? <Yes>\n"
+read -p "Press ENTER to Continue"
 sudo raspi-config
 
 echo "Part 1 done!"
