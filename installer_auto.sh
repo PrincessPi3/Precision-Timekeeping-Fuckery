8a38b6843fa62e2c035edac3b7ae737c423e9415
PrincessPi3 <human.bagel@gmail.com>
2025-06-14 03:10:44 -0600

    1.2-dev

--- installer_auto.sh
+++ installer_auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # usage
 ##  curl -s https://raw.githubusercontent.com/PrincessPi3/Precision-Timekeeping-Fuckery/refs/heads/main/installer_auto.sh?nocache=$RANDOM | $SHELL
-set -e
+# set -e
 
 if [ ! -z $SUDO_USER ]; then
     username=$SUDO_USER
