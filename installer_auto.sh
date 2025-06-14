3eab1b5b262f6805b5e47f51293fe8ae1b4e8016
PrincessPi3 <human.bagel@gmail.com>
2025-06-13 18:33:54 -0600

    1749861234

--- installer_auto.sh
+++ installer_auto.sh
@@ -15,4 +15,3 @@ cd /home/$username
 git clone https://github.com/PrincessPi3/Precision-Timekeeping-Fuckery.git
 cd /home/$username/Precision-Timekeeping-Fuckery
 echo "installer_auto.sh complete" > ./status.txt
-# bash installer1.sh
